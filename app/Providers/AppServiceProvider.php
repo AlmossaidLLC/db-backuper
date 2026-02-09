4eb2bbfb7d90101d97cdfca60c1b3f3d91591900
AbdelilahEzzouini <abdelilah.ezzouini@gmail.com>
2026-02-09 14:27:11 +0100

    feat: Update Dockerfile and AppServiceProvider for HTTPS asset URL handling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Force HTTPS asset URLs when APP_URL uses HTTPS
+        if (str_starts_with(config('app.url'), 'https://')) {
+            $this->app['config']->set('app.asset_url', config('app.url'));
+        }
     }
 
     /**
@@ -19,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $appUrl = config('app.url');
+        if (str_starts_with($appUrl, 'https://')) {
+            URL::forceScheme('https');
+            URL::forceRootUrl($appUrl);
+        }
     }
 }
