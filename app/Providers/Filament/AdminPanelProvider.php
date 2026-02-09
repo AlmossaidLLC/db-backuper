f958a22e03e7042af0d0260b80fda62c4675d6be
AbdelilahEzzouini <abdelilah.ezzouini@gmail.com>
2026-02-09 12:41:12 +0100

    fix: Remove unused FilamentAwinTheme plugin from AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -13,7 +13,6 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Resma\FilamentAwinTheme\FilamentAwinTheme;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -50,10 +49,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
 
-            ])
-            ->plugins([
-                FilamentAwinTheme::make()
-                    ->primaryColor(Color::Indigo),
             ])
             ->middleware([
                 EncryptCookies::class,
